704c7e9e597fda0db6b7d9f44bdfcf33822870d5
drisyaathul <drisyak809@gmail.com>
2023-07-19 13:11:53 +0800

    Added: Checking the number is Even or Odd by using If condition

---
+++ src/com/bridgelabz/BasicCorePrograms/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int number = scanner.nextInt();
+
+        if (number%2 == 0) {
+            System.out.println(number+ " is Even Number.");
+        } else
+            System.out.println(number+ " is Odd Number ");
+    }
+}
